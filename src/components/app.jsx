32563d8f3111857034ee8f0c1c962a0d9a9c1471
Camille Schull <camille.schull@gmail.com>
2021-01-19 11:53:11 +0100

    React section complete

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import CityList from '../containers/city_list';
+import City from '../containers/city';
+
+
+// const App = () => {
+//   return (
+//     <div className="app">
+//       <p>React + Redux starter</p>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+const cities = [
+  { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
+  { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
+  { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
+];
 
 const App = () => {
   return (
     <div className="app">
-      <p>React + Redux starter</p>
+      <CityList cities={cities} />
+      {console.log(cities[0].name)}
+      <City selectedCity={cities[0]} />
     </div>
   );
 };
