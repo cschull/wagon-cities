a8c8bdda40ca30f00c6727d4146ddfea24fb0c35
Camille Schull <camille.schull@gmail.com>
2021-01-19 14:32:16 +0100

    removed react app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,29 +2,11 @@ import React from 'react';
 import CityList from './city_list';
 import ActiveCity from '../containers/active_city';
 
-
-// const App = () => {
-//   return (
-//     <div className="app">
-//       <p>React + Redux starter</p>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-const cities = [
-  { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
-  { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
-  { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
-];
-
 const App = () => {
   return (
     <div className="app">
-      <CityList cities={cities} />
-      {console.log(cities[0].name)}
-      <ActiveCity selectedCity={cities[0]} />
+      <CityList />
+      <ActiveCity />
     </div>
   );
 };
